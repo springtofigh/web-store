924efc0cd5701208a0a777e981b25ce7ad60216c
BT <bahartofigh@gmail.com>
2023-02-09 18:16:09 +0330

    add media query for all components.

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import styles from './Products.module.css';
+import styles from './NotFound.module.css';
 
 class NotFound extends Component {
     render() {
